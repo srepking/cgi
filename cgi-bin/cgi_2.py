52764790470fe7800cb5fcb1b3475d46f15259c4
srepking <shanerepking@gmail.com>
2020-09-23 21:45:21 -0700

    Lesson 4 Activity Submission

--- cgi-bin/cgi_2.py
+++ cgi-bin/cgi_2.py
@@ -5,7 +5,7 @@ cgitb.enable()
 import os
 import datetime
 
-
+dt = datetime.datetime.today()
 default = "No Value Present"
 
 
@@ -23,10 +23,10 @@ body = """<html>
 </body>
 </html>""".format(
     software=os.environ.get('SERVER_SOFTWARE', default),
-    script='aaaa',
-    month='bbbb',
-    date='cccc',
-    year='dddd',
-    client_ip='eeee'
+    script=os.environ.get('SCRIPT_NAME', default),
+    month=dt.strftime('%b'),
+    date=dt.day,
+    year=dt.year,
+    client_ip=os.environ.get('REMOTE_ADDR', default)
 )
 print(body)
--- cgi-bin/cgi_sums.py
+++ cgi-bin/cgi_sums.py
@@ -3,7 +3,22 @@ import cgi
 import cgitb
 
 cgitb.enable()
+nums_to_sum = []
+sum = 0
 
 print("Content-type: text/plain")
-print()
-print("Your job is to make this work")
+print("")
+form = cgi.FieldStorage()
+
+for x in form:
+    nums_to_sum += form.getvalue(x, None)
+
+try:
+    for number in nums_to_sum:
+        add_num = int(number)
+        sum += add_num
+        body = "The sum of Operands is: {} ".format(sum)
+except (ValueError, TypeError):
+    body = "Unable to calculate sum of non-integers"
+
+print(body)
